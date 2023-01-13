b312ad04f86ae65fad784fb4a04139fc30c3a4b6
Wajeeha <106020730+Wajeeha-Kashif@users.noreply.github.com>
2023-01-13 22:09:19 +0500

    Add files via upload

--- ecommerce/controllers/billsController.js
+++ ecommerce/controllers/billsController.js
@@ -1,1 +1,23 @@
+import Bills from "../models/billsModel.js"
 
+export const getBillsController = async(req,res) =>{
+    try{    
+        const bills = await Bills.find();
+        res.send(bills);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+export const addBillsController = async(req, res) =>{
+    try{
+        const newBills = new Bills(req.body);
+        await newBills.save();
+        res.send("Bill created successfully!");
+
+    }catch(error){
+        console.log(error);
+    }
+}
---
+++ ecommerce/controllers/userController.js
@@ -0,0 +1,33 @@
+import User from "../models/userModel.js"
+
+//for login
+export const loginController = async(req,res) =>{
+    try{    
+        const {userId, password} = req.body;
+        const user = await User.findOne({userId, password});
+       if(user){
+        res.status(200).send(user);
+       }
+       else{
+        res.json({
+            message: "Login Failed",
+            user,
+        });
+       }
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//for register
+export const registerController = async(req, res) =>{
+    try{
+        const newUser = new User({...req.body, verified: true});
+        await newUser.save();
+        res.status(200).send("User registered successfully!");
+
+    }catch(error){
+        console.log(error);
+    }
+}
